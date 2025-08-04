383403a2896b360bd69c472e8ece6422406e9193
Kakashi-dev <kakashi31092009@gmail.com>
2025-08-04 11:37:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-echo "🚀 Starting Bestie Bot..."
-
-# Optional: activate venv if using one
-# source venv/bin/activate
-
-# Optional: update/install dependencies (uncomment if needed)
-# pip install -r requirements.txt
-
-# Start your bot
-python bestie.py
+python-3.10.13 bestie.py
